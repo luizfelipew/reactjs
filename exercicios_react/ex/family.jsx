d9901df3b044b3709fee6e4e66b07e8a6383c746
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-03-27 13:01:01 -0300

    Passando propriedades para componentes filhos parte 2

--- exercicios_react/ex/family.jsx
+++ exercicios_react/ex/family.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { childrenWithProps } from '../utils/reactUtils'
+
 
 export default props => (
     <div>
         <h1>Familia</h1>
-        { React.cloneElement(props.children, {...props}) }
+        {childrenWithProps(props.children, props)}
     </div>
 )
